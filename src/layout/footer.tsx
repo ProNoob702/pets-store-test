38242da1e7159b64885743519ac40ba1b0337acc
taha.seddik1992@gmail.com <taha.seddik1992@gmail.com>
2022-05-07 17:00:22 +0100

    layout is ok

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const AppFooter: React.FC<{}> = () => {
-  return <div>FOoter</div>;
+  return (
+    <div className="p-4 border-t text-center">
+      Copyright © 2022 Pets-store by Taha seddik
+    </div>
+  );
 };
--- src/layout/navBar.tsx
+++ src/layout/navBar.tsx
@@ -3,7 +3,12 @@ export const NavBar: React.FC<{}> = () => {
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-sky-500 ">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <div className="flex w-full items-center relative">
+            <img
+              className="w-12 mr-1"
+              alt="logo"
+              src="https://static.thenounproject.com/png/1859362-200.png"
+            />
             <span className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white">
               Pets-Store
             </span>
